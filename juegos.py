7475336df2e461574f5233444af479be54abbb35
Jesus Manuel Monroy Quintero <l181061128@mulege.tecnm.mx>
2021-11-16 01:08:17 -0700

    Se agregó dados a función menú

--- juegos.py
+++ juegos.py
@@ -15,20 +15,33 @@ def tombola():
         else:
             print('Sigue paticipando')
 
+# Juego de dados
+def dados():
+        dado1 = random.randint(1, 6)
+        dado2 = random.randint(1, 6)
+        print("Primer dado: ", dado1)
+        print("Segundo dado: ", dado2)
+        suma = dado1 + dado2
+        if suma == 7:
+            print("Ganaste!!")
+        else:
+            print("Mejor suerte para la próxima")
+
 # Menu de juego
 def menu():
     i = 0
     while True:
         print("\nSeleciona el juego:")
         print("1. Tombola")
-        print("2. Juego nuevo")
+        print("2. Dados")
         print("3. Salir")
         i = int(input("Ingresa seleccion: "))
         if i == 1:
             print("\nTombola")
             tombola()
         elif i == 2:
-            print("Juego nuevo")
+            print("\nDatos que suman 7")
+            dados()
         elif i == 3:
             break
         else:
